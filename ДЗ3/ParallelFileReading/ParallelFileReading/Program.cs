da1327e3ce10a7177d574be6f97f7a7f0f769f82
T1m-cpp <lehak1999@mail.ru>
2025-05-13 18:35:39 +0300

    updated dz3

--- ДЗ3/ParallelFileReading/ParallelFileReading/Program.cs
+++ ДЗ3/ParallelFileReading/ParallelFileReading/Program.cs
@@ -10,6 +10,7 @@ int workersCount = 3;
 
 var timer = new Stopwatch();
 timer.Start();
+//Считаем количество пробелов в 3-х файлах 3-мя тасками 
 int count = ParallelFileReader.ReadFiles("files", workersCount);
 timer.Stop();
 Console.WriteLine($"Время обработки 3-х файлов: {timer.Elapsed}");
@@ -25,6 +26,7 @@ RandomFileGenerator.GenerateRandomFiles("files", 20, 1000, 100000);
 workersCount = Environment.ProcessorCount; //Получаем кол-во потоков нашего процессора
 
 timer.Start();
+//Считаем количество пробелов во всех файлах числом тасок, равным колечесту потоков нашего процессора
 count = ParallelFileReader.ReadFiles("files", workersCount);
 timer.Stop();
 Console.WriteLine($"Время обработки всех файлов в папке: {timer.Elapsed}");
